5c11fd7ee54a04655c6eaffa976c39dc816b2a17
Bryan Wodi <talk2kamp@gmail.com>
2016-10-23 17:31:19 -0500

    fixed bugs

--- main.cpp
+++ main.cpp
@@ -233,8 +233,10 @@ static std::vector<cv::Point> get_points_in_clockwise_order(const cv::Mat& frame
     cv::Mat img_canny;
     static std::vector<std::vector<cv::Point> > contours; 
     static std::vector<cv::Point> approx_poly_points;
-    static std::vector<cv::Point> ordered_points[4];
+    static std::vector<cv::Point> ordered_points;
     static std::vector<cv::Vec4i> hierarchy;
+
+    ordered_points[0] = cv::Point(-1,-1);
 
     //canny, then check if it's a rectangle and check if a circle's in it
     cv::Canny(frame,img_canny,canny_threshold,canny_threshold*canny_ratio,canny_kernel_size);
@@ -306,7 +308,7 @@ static cv::Point get_centre_of_frame(const cv::Mat& frame)
 
     frame_points = get_points_in_clockwise_order(frame);
 
-    if ( frame_points[0].x != 0 && frame_points[0].y != 0 )
+    if ( frame_points[0].x != -1 && frame_points[0].y != -1 )
     {
         sum_y = 0.0;
         sum_x = 0.0;
